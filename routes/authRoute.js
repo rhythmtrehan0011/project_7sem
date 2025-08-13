390b015fb057b0de34b195064cb42e124485b993
Rhythm <rhythmtrehan0011@gmail.com>
2025-08-13 22:31:00 +0530

    Revert "user signup"
    This reverts commit b8441007b9a263dc4a57a409b44f963ae5e60e91.

--- routes/authRoute.js
+++
@@ -1,5 +0,0 @@
-const express=require('express');
-const{signUpUser}=require("../controller/authController");
-const router=express.Router();
-router.post("/signup",signUpUser);
-module.exports=router;
